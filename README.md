f52e67e999718bdf28e8e6610a6cebf294bd7006
João H. <83355344+joaostack@users.noreply.github.com>
2026-02-05 14:09:10 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# stacknvim
-A neovim configuration  based in AstroNVim
+# AstroNvim Template
+
+## Dependencies
+- Python3+
+- NodeJS + npm/npx
+- Compiler (TDM-GCC is a good option)
+
+**NOTE:** This is for AstroNvim v5+
+
+A template for getting started with [AstroNvim](https://github.com/AstroNvim/AstroNvim)
+
+## 🛠️ Installation
+
+#### Clone the repository
+
+```shell
+git clone git@github.com:joaostack/WinDev-Environment.git "%LOCALAPPDATA%\nvim"
+```
+
+#### Start Neovim
+
+```shell
+nvim
+```
+
+
+
---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,30 @@
+return {
+  "seblyng/roslyn.nvim",
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      filewatching = "auto",
+      broad_search = true,
+      silent = true,
+    },
+  },
+
+  config = function()
+    vim.lsp.config("roslyn", {
+      on_attach = function() print "This will run when the server attaches!" end,
+      settings = {
+        ["csharp|inlay_hints"] = {
+          csharp_enable_inlay_hints_for_implicit_object_creation = true,
+          csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        },
+        ["csharp|code_lens"] = {
+          dotnet_enable_references_code_lens = true,
+        },
+      },
+    })
+
+    require("roslyn").setup()
+  end,
+}
