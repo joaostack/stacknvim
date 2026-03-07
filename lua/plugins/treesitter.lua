650be8d13ab760ef2b5992eb2507dade117e2d96
João Henryque <83355344+joaostack@users.noreply.github.com>
2026-03-07 01:17:35 -0300

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"c_sharp",
+			-- add more arguments for adding more treesitter parsers
+		},
+	},
 }
