2ec71a30361521fe7948e0c18100f6797ef9700a
joaostack <83355344+joaostack@users.noreply.github.com>
2026-06-11 21:44:53 -0300

    update

--- lua/community.lua
+++ lua/community.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 }
